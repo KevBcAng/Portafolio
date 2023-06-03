aa8ecd8b5b4fb8f28e843b81dc37d82f8691ddeb
KevBcAng <kevi.baca18@gmail.com>
2023-06-03 12:38:05 -0500

    Incluye config de webpack

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import './assets/js/main';
+import './assets/js/fixed';
+import '../node_modules/eins-modal/dist/js/eins-modal.min.js';
+//* Importando archivo de css index que, esto solo funciona enn webpack porque asi esta diseniado. 
+import './assets/css/index.css';
